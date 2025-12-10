229987ab08a935c4cd1e6f1c3f6fa2446ce6d06c
jhonxxz06 <jhonxxtiktok@gmail.com>
2025-12-09 22:13:06 -0300

    Atualização do arquivo api do front

--- frontend/teste-mcp/src/services/api.js
+++ frontend/teste-mcp/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://projetocleanair.onrender.com';
+const API_URL = 'https://projetocleanair.onrender.com/api';
 
 // Helpers
 const getToken = () => localStorage.getItem('token');
